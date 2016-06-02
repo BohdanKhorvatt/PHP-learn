3b47d55c4d25bf17a62fe063193ae5f57f88c148
Bogdan Khorvat <fedorbogdanovf@yandex.ru>
2016-06-02 19:43:06 +0300

    function

--- index.php
+++ index.php
@@ -184,6 +184,25 @@
 				} while ($z < 5);
 				
 
+				# function
+
+				function printWords ($word, $x, $y) {
+					$word = math ($x, $y);
+					echo $word;
+				}
+
+				function math ($first, $second) {
+					$summa = $first + $second;
+					return $summa;
+				}
+
+				$x = 12;
+				$y = 35;
+
+				$sum = math ($x, $y);
+				echo "<hr>";
+
+				printWords ($sum, $x, $y);
 
 
 
